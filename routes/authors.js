0481d6b22ce5c4b4c936cc95d36dc6a4ca80111b
CodaarX <micahelisesele@gmail.com>
2021-07-16 19:51:33 +0100

    edited author.js filet

--- routes/authors.js
+++ routes/authors.js
@@ -25,7 +25,7 @@ router.get('/new', (req, res) => {
   res.render('authors/new', { author: new Author() })
 })
 
-// Create Author Route
+// Create Author Route 
 router.post('/', async (req, res) => {
   const author = new Author({
     name: req.body.name
